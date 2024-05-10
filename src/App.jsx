4625527ad9dc5955eae59a8847723d30a57fb2dd
Ayoush <ayoushchourasia1418@gmail.com>
2024-05-10 16:04:07 +0530

    Added navbar plus added states and recoil management to change tabname from sidebar.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import NavBar from "./components/Navbar/navbar"
 import SideBar from "./components/Sidebar/sidebar"
 import Content from "./components/Tabs/content"
+import { RecoilRoot } from "recoil";
 
 function App() {
   return (
+      <RecoilRoot>
       <div className="flex flex-row">
         <SideBar></SideBar>
           <div className="flex flex-col w-full p-0 h-screen">
@@ -11,6 +13,7 @@ function App() {
             <Content></Content> 
           </div>
       </div>
+      </RecoilRoot>
   )
 }
 
--- src/components/Sidebar/sidebar.jsx
+++ src/components/Sidebar/sidebar.jsx
@@ -1,7 +1,17 @@
+import { useSetRecoilState } from "recoil";
+import { tabName } from "../../atoms/navbar";
+
 export default function SideBar() {
+  const setTabName = useSetRecoilState(tabName);
   return (
     <div className="bg-white h-screen max-w-[300px] min-w-[200px] w-[20%] py-4 shadow-md sticky top-0 border border-black">
-      SideBar
+      <div>
+        Logo
+      </div>
+      <div>
+        Pages
+      {/* <button onClick={() => setTabName("github")}>Click Here</button> */}
+      </div>
     </div>
   );
 }
